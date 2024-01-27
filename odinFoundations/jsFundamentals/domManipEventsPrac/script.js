4368bb066e02da40154dbf86ecd0b04b59313267
koleada <koleniz.da@gmail.com>
2024-01-26 23:58:33 -0500

    Good progress through the course, only a couple more lessons!

---
+++ odinFoundations/jsFundamentals/domManipEventsPrac/script.js
@@ -0,0 +1,34 @@
+// reference the input 
+const input = document.querySelector("#usrIn");
+
+// reference the ul
+const list = document.querySelector("#list");
+
+//select button and add onclick listener. When clicked, create a new li set the innerHTML of it to the value of the input. Append the li to the ul.
+document.querySelector("#btn").addEventListener("click", () => {
+
+
+    //create new li and set the innerHTML of it to the value of the input
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+
+    //create a delete button, add margin to seperate from the user input, add onclick listener to remove the li from the list if delete button is clicked 
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.style.margin = "15px";
+    deleteBtn.addEventListener("click", function(e) {
+        list.removeChild(e.target.parentElement);
+    });
+
+    //append btn to li
+    li.appendChild(deleteBtn);
+
+    //append li to ul
+    list.appendChild(li);
+
+    //alert the user the value is being added.
+    alert("Adding " + input.value + " to the list...");
+
+    //reset the value of the input to an empty string
+    input.value = "";
+});
--- odinFoundations/jsFundamentals/jsEvents/eventPrac.js
+++ odinFoundations/jsFundamentals/jsEvents/eventPrac.js
@@ -1,15 +1,14 @@
-
-const btn = document.querySelector('#btn')
-const btn2 = document.querySelector("#btn2")
-  // METHOD 2
+const btn = document.querySelector("#btn");
+const btn1 = document.querySelector("#btn");
+const btn2 = document.querySelector("#btn2");
+// METHOD 2
 btn.onclick = () => alert("Hello World");
-  
-  // METHOD 3
-btn2.addEventListener('click', () => {
-    alert("Hello World");
-});
 
-btn.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
+// METHOD 3
+btn.onclick = () => alert("Hello World");
+
+btn2.addEventListener("click", function(e) {
+  e.target.style.background = "blue";
+  alert("I think I'm gonna turn blue!");
+  console.log(e.target);
 });
-  
